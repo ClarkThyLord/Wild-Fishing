dd29b094401f5a6f84bcc3e97b4128f2244fc64f
ClarkThyLord <ccpotential@gmail.com>
2022-01-20 19:53:09 -0800

    Improve MainMenu

--- src/scenes/main_menu/main_menu.gd
+++ src/scenes/main_menu/main_menu.gd
@@ -6,3 +6,11 @@ extends Control
 ## Private Methods
 func _on_Start_pressed():
 	Session.open_map()
+
+
+func _on_Settings_pressed():
+	Settings.show()
+
+
+func _on_GitHub_pressed():
+	OS.shell_open("https://github.com/ClarkThyLord/Wild-Fishing")
