68268a467f46f4aaf353dd92736c04ba5df8764f
ClarkThyLord <ccpotential@gmail.com>
2022-01-20 19:37:43 -0800

    Setup Map areas

--- src/scenes/map/map.gd
+++ src/scenes/map/map.gd
@@ -4,15 +4,26 @@ extends Node
 
 
 ## Private Methods
-func _on_Shop_input_event(
-		viewport  : Viewport, event : InputEvent, shape_idx : int):
+func _on_Area_input_event(
+		viewport : Viewport,
+		event : InputEvent,
+		shape_idx : int,
+		area_name : String) -> void:
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 	if event is InputEventMouseButton:
 		if event.pressed:
-			Session.open_shop()
+			return
+		
+		match area_name:
+			"shop":
+				Session.open_shop()
+			_:
+				Session.open_level(area_name)
 
 
-func _on_Bay_input_event(
-		viewport  : Viewport, event : InputEvent, shape_idx : int):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			Session.open_level("Bay")
+func _on_Area_mouse_entered():
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
+
+func _on_Area_mouse_exited():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
