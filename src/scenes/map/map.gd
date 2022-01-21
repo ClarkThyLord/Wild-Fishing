50d52f78d4f19d0851c2ceea7a608cf1fda02332
ClarkThyLord <ccpotential@gmail.com>
2022-01-20 19:46:13 -0800

    Make Map more responsive

--- src/scenes/map/map.gd
+++ src/scenes/map/map.gd
@@ -4,26 +4,30 @@ extends Node
 
 
 ## Private Methods
+func _set_area_text_color(area_name : String, color : Color) -> void:
+	var label : Label = get_node_or_null(area_name + "/Label")
+	if is_instance_valid(label):
+		label.modulate = color
+
+
 func _on_Area_input_event(
 		viewport : Viewport,
 		event : InputEvent,
 		shape_idx : int,
 		area_name : String) -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	_set_area_text_color(area_name, Color("858585"))
 	if event is InputEventMouseButton:
 		if event.pressed:
 			return
 		
 		match area_name:
-			"shop":
+			"Shop":
 				Session.open_shop()
 			_:
 				Session.open_level(area_name)
 
 
-func _on_Area_mouse_entered():
-	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
-
-
-func _on_Area_mouse_exited():
+func _on_Area_mouse_exited(area_name : String):
+	_set_area_text_color(area_name, Color.white)
 	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
