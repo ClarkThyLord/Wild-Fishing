4131c8e73e64efcabf9621b51aa452b67ba5a153
ClarkThyLord <ccpotential@gmail.com>
2022-01-23 00:51:27 -0800

    Block Map areas

--- src/scenes/map/map.gd
+++ src/scenes/map/map.gd
@@ -11,12 +11,21 @@ onready var money : Label = get_node("Texture/InfoPanel/Money")
 ## Built-In Virtual Methods
 func _ready() -> void:
 	money.text = "$     %07d" % Session.money
+	
+	if not "Boating Lessons" in Session.inventory:
+		_get_area_text("Bay").text = "?"
+	if not "Sailing Lessons" in Session.inventory:
+		_get_area_text("ShallowWaters").text = "?"
 
 
 
 ## Private Methods
+func _get_area_text(area_name : String) -> Label:
+	return get_node_or_null(area_name + "/Label") as Label
+
+
 func _set_area_text_color(area_name : String, color : Color) -> void:
-	var label : Label = get_node_or_null(area_name + "/Label")
+	var label := _get_area_text(area_name)
 	if is_instance_valid(label):
 		label.modulate = color
 
@@ -35,6 +44,14 @@ func _on_Area_input_event(
 		match area_name:
 			"Shop":
 				Session.open_shop()
+			"Bay":
+				if not "Boating Lessons" in Session.inventory:
+					return
+				continue
+			"ShallowWaters":
+				if not "Sailing Lessons" in Session.inventory:
+					return
+				continue
 			_:
 				Session.open_level(area_name)
 
