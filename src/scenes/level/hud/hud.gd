007d944064ae9ab26f49f0e30c7c81c6873d1e80
ClarkThyLord <ccpotential@gmail.com>
2022-01-22 21:19:45 -0800

    Add Level HUD options

--- src/scenes/level/hud/hud.gd
+++ src/scenes/level/hud/hud.gd
@@ -1,9 +1,23 @@
 extends CanvasLayer
-## Level HUD Class
+## Level HUD
+
+
+
+## Signals
+signal resting
+
+
+
+## Private Variables
+var _depth := 0.0
 
 
 
 # OnReady Variables
+onready var map : Button = get_node("Options/HBoxContainer/Map")
+
+onready var rest : Button = get_node("Options/HBoxContainer/Rest")
+
 onready var money : Label = get_node("InfoPanel/Money")
 
 onready var depth : Label = get_node("InfoPanel/Depth")
@@ -23,10 +37,16 @@ func update_money(value : float = Session.money) -> void:
 
 
 func update_depth(value : float) -> void:
-	if not is_instance_valid(depth):
-		return
+	_depth = value
 	
-	depth.text = "%.2f ft." % value
+	if is_instance_valid(map):
+		map.disabled = _depth > 0.0
+	
+	if is_instance_valid(rest):
+		rest.disabled = _depth > 0.0
+	
+	if is_instance_valid(depth):
+		depth.text = "%.2f ft." % value
 
 func update_depth_max(value : float) -> void:
 	if not is_instance_valid(depth_max):
@@ -37,5 +57,16 @@ func update_depth_max(value : float) -> void:
 
 
 ## Private Methods
+func _on_Map_pressed():
+	Session.open_map()
+
+
+func _on_Rest_pressed():
+	if _depth > 0.0:
+		return
+	
+	emit_signal("resting")
+
+
 func _on_Settings_pressed():
 	Settings.show()
