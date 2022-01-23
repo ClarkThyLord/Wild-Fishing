87580749624d608955405b4c58c156e688412a45
ClarkThyLord <ccpotential@gmail.com>
2022-01-23 11:10:58 -0800

    Value fish

--- src/scenes/level/objects/fish/fish.gd
+++ src/scenes/level/objects/fish/fish.gd
@@ -50,21 +50,28 @@ func set_fish(value : int) -> void:
 	fish = value
 	
 	match fish:
-		9, 15, 6, 7, 8:
+		9, 15, 6, 7, 8, 16, 17, 18, 19, 20, 79, 80, 81, 82, 83, 84:
 			# SMALL
 			set_worth(15)
-		10, 11, 12:
+		10, 11, 12, 30, 33, 34, 57, 58, 59:
 			# MEDIUM
 			set_worth(25)
-		0, 1, 2, 3, 5:
+		0, 1, 2, 3, 5, 21, 22, 23, 24, 25, 51, 60, 61, 85, 86, 87:
 			# BIG
 			set_worth(40)
-		14:
+		14, 26, 27, 28, 29, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,\
+				48, 49, 50, 71, 72, 73, 74, 75:
 			# SMALL EXCOTIC
 			set_worth(50)
-		13:
+		13, 31, 32, 52, 53, 54, 55, 56, 69, 70:
 			# MEDIUM EXCOTIC
-			set_worth(10)
+			set_worth(100)
+		62, 63, 64, 65, 66, 67, 68, 88, 89:
+			# BIG EXCOTIC
+			set_worth(150)
+		76, 77, 78:
+			# RARE
+			set_worth(200)
 		_:
 			set_worth(stepify(100 * randf(), 10))
 	
