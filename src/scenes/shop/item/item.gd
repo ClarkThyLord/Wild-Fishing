1d32d78e6adf2c58d52dcf98e88d7f24620997b6
ClarkThyLord <ccpotential@gmail.com>
2022-01-22 19:15:46 -0800

    Display Item owned and using status

--- src/scenes/shop/item/item.gd
+++ src/scenes/shop/item/item.gd
@@ -14,6 +14,10 @@ signal used
 ## Exported Variables
 export var owned := false setget set_owned
 
+export var usable := false setget set_usable
+
+export var using := false setget set_using
+
 export var item_texture : Texture setget set_item_texture
 
 export var item_name := "" setget set_item_name
@@ -46,6 +50,8 @@ onready var use : Button = get_node("HBoxContainer/VBoxContainer2/Use")
 
 ## Built-In Virtual Methods
 func _ready() -> void:
+	set_owned(owned)
+	set_usable(usable)
 	set_item_texture(item_texture)
 	set_item_name(item_name)
 	set_item_price(item_price)
@@ -62,7 +68,21 @@ func set_owned(value : bool) -> void:
 	if is_instance_valid(buy):
 		buy.visible = not owned
 	if is_instance_valid(use):
-		use.visible = not owned
+		use.visible = owned
+
+
+func set_usable(value : bool) -> void:
+	usable = value
+	
+	if is_instance_valid(use):
+		use.disabled = not usable
+
+
+func set_using(value : bool) -> void:
+	using = value
+	
+	if is_instance_valid(use):
+		use.text = "USING" if using else "USE"
 
 
 func set_item_texture(value : Texture) -> void:
@@ -96,12 +116,25 @@ func set_item_price(value : int) -> void:
 func set_item(item_name : String, item : Item) -> void:
 	_item = item
 	
-	set_item_texture(item.get_texture())
+	set_item_texture(_item.get_texture())
 	set_item_name(item_name)
-	set_item_description(item.get_description())
-	set_item_price(item.get_price())
+	set_item_description(_item.get_description())
+	set_item_price(_item.get_price())
 	
 	set_owned(item_name in Session.inventory)
+	if owned:
+		match _item.get_item_type():
+			Item.ItemType.BOAT:
+				set_usable(true)
+				set_using(item_name == Session.boat)
+			Item.ItemType.LINE:
+				set_usable(true)
+				set_using(item_name == Session.line)
+			Item.ItemType.HOOK:
+				set_usable(true)
+				set_using(item_name == Session.hook)
+			_:
+				set_usable(false)
 
 
 ## Private Methods
